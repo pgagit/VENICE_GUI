36e4f09f566f9cb35c3a83677b060105ba9c919c
pgagit <alexfpga1@gmail.com>
2022-05-20 04:44:37 +0200

    init

---
+++ CMakeFiles/myprogram.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myprogram.dir/gui.c.o"
+  "CMakeFiles/myprogram.dir/gui.c.o.d"
+  "myprogram"
+  "myprogram.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myprogram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
